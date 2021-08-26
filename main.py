c23a25031d893e1f5aa338fc1dcd8d94170c8663
hsfzxjy <hsfzxjy@gmail.com>
2021-08-26 13:21:47 +0800

    Fix

--- main.py
+++ main.py
@@ -53,18 +53,66 @@ def _extract_form_values(root):
     return payload
 
 
+def get_validatecode(session: requests.Session) -> str:
+    import re
+    import pytesseract
+    from PIL import Image
+    from io import BytesIO
+
+    for attempts in range(20):
+        response = session.get(
+            "https://passport.ustc.edu.cn/validatecode.jsp?type=login"
+        )
+        stream = BytesIO(response.content)
+        image = Image.open(stream)
+        text = pytesseract.image_to_string(image)
+        codes = re.findall(r"\d{4}", text)
+        if len(codes) == 1:
+            break
+    return codes[0]
+
+
 def login(session: requests.Session, username: str, password: str):
+    session.headers.update(
+        {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+            "Origin": "https://passport.ustc.edu.cn",
+            "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-User": "?1",
+            "Sec-Fetch-Dest": "document",
+            "Cache-Control": "max-age=0",
+            "Upgrade-Insecure-Requests": "1",
+        }
+    )
+    session.cookies.set("lang", "zh")
+    response = session.get(
+        "https://weixine.ustc.edu.cn/2020/caslogin",
+        headers={"Referer": "https://weixine.ustc.edu.cn/2020/login"},
+    )
+    root = document_fromstring(response.text)
+    input = root.cssselect("input[name=CAS_LT]")
+    CAS_LT = input[0].value
+
     response = session.post(
         "https://passport.ustc.edu.cn/login",
         data={
             "model": "uplogin.jsp",
             "service": "https://weixine.ustc.edu.cn/2020/caslogin",
+            "CAS_LT": CAS_LT,
             "warn": "",
-            "showCode": "",
+            "showCode": "1",
             "username": username,
             "password": password,
             "button": "",
+            "LT": get_validatecode(session),
         },
+        headers={
+            "Referer": "https://passport.ustc.edu.cn/login?service=https://weixine.ustc.edu.cn/2020/caslogin",
+        },
+        allow_redirects=True,
     )
     return response
 
@@ -72,7 +120,6 @@ def login(session: requests.Session, username: str, password: str):
 def report_health(response: requests.Response):
     root = document_fromstring(response.text)
     payload = _extract_form_values(root)
-    # print(payload)
     return session.post("https://weixine.ustc.edu.cn/2020/daliy_report", data=payload)
 
 
@@ -80,10 +127,12 @@ if __name__ == "__main__":
     IDENT = os.getenv("IDENT")
     session = requests.Session()
     r = login(session, *IDENT.split(":"))
-    # print("logined")
+    print("logined")
     r = report_health(r)
-    # print(r.status_code)
+    print(r.status_code)
     if r.status_code != 200 or "上报成功" not in r.text:
         # print(r.text)
-        print('error')
+        print("error")
         sys.exit(1)
+    else:
+        print("OK")
